d46de3db919b97eef55422fcf856c81ecbc2a9a6
Anastasia Kovalyshyn <anastasiyakovalyshyn@gmail.com>
2019-09-03 10:40:23 +0300

    make one module

--- content/src/main/scala/Content.scala
+++
@@ -1,5 +0,0 @@
-object Content extends App{
-  def printLibraryContent(): Unit = {
-    println("Library consist of nothing now")
-  }
-}
--- content/src/test/scala/test.scala
+++
@@ -1,3 +0,0 @@
-object test extends App {
-  println("Hello from content.test.scala.test")
-}
