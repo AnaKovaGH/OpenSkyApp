5567674bf5eac6dbdcdb6cda6d434b95026f93a3
Anastasia Kovalyshyn <anastasiyakovalyshyn@gmail.com>
2019-09-13 12:12:46 +0300

    fix sending to Kakfa

--- build.sbt
+++ build.sbt
@@ -17,9 +17,9 @@ lazy val root = (project in file("."))
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.25",
   "com.typesafe" % "config" % "1.3.4",
+  "org.apache.kafka" % "kafka_2.12" % "2.3.0",
+  "ch.qos.logback" % "logback-classic" % "1.2.3" % Runtime //SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
 
-
-  "org.apache.kafka" % "kafka_2.12" % "2.3.0"
 )
   
 
--- src/main/scala/actors/SendingKafkaActor.scala
+++ src/main/scala/actors/SendingKafkaActor.scala
@@ -2,8 +2,9 @@ package actors
 
 import akka.actor.Actor
 import messages.{CompleteWork, SendDataToKafkaMessage}
-
 import java.util.Properties
+
+import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 
@@ -14,25 +15,26 @@ class SendingKafkaActor() extends Actor {
       context.parent ! CompleteWork
     case _ => println("Unknown message. Did not start sending data. SendingKafkaActor.")
   }
+
   def sendDataToKafka(data: String): String = {
+    val config: Config = ConfigFactory.load("OpenSky.conf").getConfig("kafkaconfig")
+
     val props:Properties = new Properties()
-    props.put("bootstrap.servers","localhost:9092")
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("acks","all")
+    props.put("bootstrap.servers", config.getString("bootstrap-servers"))
+    props.put("key.serializer", config.getString("key-serializer"))
+    props.put("value.serializer", config.getString("value-serializer"))
+    props.put("acks", config.getString("acks"))
 
     val producer = new KafkaProducer[String, String](props)
     val topic = "openskystates"
     try {
       val record = new ProducerRecord[String, String](topic, data)
       val metadata = producer.send(record)
-      println(metadata)
     }catch{
-      case e:Exception => e.printStackTrace()
+      case error:Exception => error.printStackTrace()
     }finally {
       producer.close()
     }
-
     "Test sending"
   }
 }
