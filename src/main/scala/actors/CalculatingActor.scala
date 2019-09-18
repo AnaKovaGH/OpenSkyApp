a00f0ddc317a6f65ada781de5beeb6f807dc6ffd
Anastasia Kovalyshyn <anastasiyakovalyshyn@gmail.com>
2019-09-18 17:30:56 +0300

    test computing

--- src/main/scala/actors/CalculatingActor.scala
+++ src/main/scala/actors/CalculatingActor.scala
@@ -2,8 +2,7 @@ package actors
 
 
 import akka.actor.{Actor, ActorSelection}
-import io.circe.Json
-
+import io.circe.{HCursor, Json}
 import messages.{CalculateDataMessage, SendDataToKafkaMessage}
 
 
@@ -12,7 +11,7 @@ class CalculatingActor() extends Actor {
 
   override def receive: Receive = {
     case CalculateDataMessage(transformedData) =>
-      val HighestAttitude: Int = findHighestAttitude(transformedData)
+      val HighestAttitude: Int = findHighestAltitude(transformedData)
       val HighestSpeed:Int = findHighestSpeed(transformedData)
       val CountOfAirplanes:Int = findCountOfAirplanes(transformedData)
       val results: String = wrapper(HighestAttitude, HighestSpeed, CountOfAirplanes) //!TEMPORARY!
@@ -20,7 +19,33 @@ class CalculatingActor() extends Actor {
     case _ => println("Unknown message. Did not start calculating data. CalculatingActor.")
   }
 
-  def findHighestAttitude(data: Json): Int = {
+  def findHighestAltitude(data: Json): Int = {
+    try {
+      val timestamp = data.findAllByKey("time").head
+
+      val cursor: HCursor = data.hcursor
+      val states= cursor.downField("states").values.get.toList  // List[Json] like ( [], [], [], [] )
+
+      var maxAltitude: Double = 0
+
+      for( item <- states ){
+        val cursor: HCursor = item.hcursor
+        val list: List[Json] = cursor.values.get.toList
+        val altitude: Json = list(7)
+
+        if (altitude.isNumber){
+          val number: Double = altitude.toString.toDouble
+          if (number > maxAltitude) {
+            maxAltitude = number
+          }
+        }
+      }
+      println(timestamp + ": " + maxAltitude)
+    }
+    catch {
+      case error: Exception => error.printStackTrace()
+    }
+
     1//!TEMPORARY!
   }
 
