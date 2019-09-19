af2b687e4d28468fe1c1ce7f5d6fa100790b437c
Anastasia Kovalyshyn <anastasiyakovalyshyn@gmail.com>
2019-09-19 14:05:59 +0300

    rewrite computing altitude and speed

--- src/main/scala/actors/CalculatingActor.scala
+++ src/main/scala/actors/CalculatingActor.scala
@@ -2,58 +2,117 @@ package actors
 
 
 import akka.actor.{Actor, ActorSelection}
+import com.typesafe.config.{Config, ConfigFactory}
 import io.circe.{HCursor, Json}
 import messages.{CalculateDataMessage, SendDataToKafkaMessage}
+
+import scala.collection.mutable.ListBuffer
 
 
 class CalculatingActor() extends Actor {
   val sendingKafkaActor: ActorSelection = context.actorSelection("/user/SupervisorActor/sendingKafkaActor")
+  val ingestingActor: ActorSelection = context.actorSelection("/user/SupervisorActor/ingestingActor")
 
   override def receive: Receive = {
     case CalculateDataMessage(transformedData) =>
-      val HighestAttitude: Int = findHighestAltitude(transformedData)
-      val HighestSpeed:Int = findHighestSpeed(transformedData)
-      val CountOfAirplanes:Int = findCountOfAirplanes(transformedData)
-      val results: String = wrapper(HighestAttitude, HighestSpeed, CountOfAirplanes) //!TEMPORARY!
-      sendingKafkaActor ! SendDataToKafkaMessage(results)
+      val data: (Json, List[Json]) = extractData(transformedData)
+
+      val HighestAltitude: Double = findHighestAltitude(data._2)
+      val HighestSpeed: Double = findHighestSpeed(data._2)
+      //val CountOfAirplanes: Int = findCountOfAirplanes(data)
+      //val results: String = wrapper(HighestAltitude, HighestSpeed, CountOfAirplanes) //!TEMPORARY!
+      sendingKafkaActor ! SendDataToKafkaMessage(HighestAltitude.toString)//SendDataToKafkaMessage(results)
+
     case _ => println("Unknown message. Did not start calculating data. CalculatingActor.")
   }
 
-  def findHighestAltitude(data: Json): Int = {
-    try {
+  def extractData(data: Json): (Json, List[Json]) = {
       val timestamp = data.findAllByKey("time").head
-
       val cursor: HCursor = data.hcursor
-      val states= cursor.downField("states").values.get.toList  // List[Json] like ( [], [], [], [] )
+      val states = cursor.downField("states").values.get.toList // List[Json] like ( [], [], [], [] )
+      (timestamp, states)
+  }
 
-      var maxAltitude: Double = 0
+  def createBufferListWithNumbers(states: List[Json], index: Int): ListBuffer[Double] = {
+    var buffer = new ListBuffer[Double]()
+    for( item <- states ) {
+      val cursor: HCursor = item.hcursor
+      val list: List[Json] = cursor.values.get.toList
+      val currentNumber: Json = list(index)
+      if (currentNumber.isNumber) {
+        buffer += currentNumber.toString.toDouble
+      }
+    }
+    buffer
+  }
 
-      for( item <- states ){
+  def findHighestAltitude(data: List[Json]): Double = {
+    try {
+      val states = data
+      val index: Int = 7
+
+      val buffer = createBufferListWithNumbers(states, index)
+      val listOfAltitudes: List[Double] = buffer.toList
+      val maxAltitude: Double = listOfAltitudes.max
+      maxAltitude
+    }
+    catch {
+      case error: Exception =>
+        error.printStackTrace()
+        1.1 //TODO: find what to return
+    }
+  }
+
+  def findHighestSpeed(data: List[Json]): Double = {
+    try {
+      val states = data
+      val index: Int = 9
+
+      val buffer = createBufferListWithNumbers(states, index)
+      val listOfSpeed: List[Double] = buffer.toList
+      val maxSpeed: Double = listOfSpeed.max
+      maxSpeed
+    }
+    catch {
+      case error: Exception =>
+        error.printStackTrace()
+        1.1 //TODO: find what to return
+    }
+  }
+
+  def findCountOfAirplanes(data: (Json, List[Json])): Int = {
+    //val config: Config = ConfigFactory.load("OpenSky.conf")
+    //val radius: String = config.getString("airportsconfig.radius")
+    //val airport1: List[Float] = config.getString("airportsconfig.airport1")
+
+    //test parameters
+    val radius: Double = 12345.5
+    val airport1: List[Float] = List(49.842957f, 24.031111f) //lat, long
+    val airport2: List[Float] = List(50.411198f, 30.446634f)
+    val listOfAiports: List[List[Float]] = List(airport1, airport2)
+
+    val timestamp = data._1
+    val states= data._2
+
+    for( airport <- listOfAiports ){
+      val lamin: Float = (airport.head - radius).toFloat
+      val lamax: Float = (airport.head + radius).toFloat
+      val lomin: Float = (airport.last - radius).toFloat
+      val lomax: Float = (airport.last + radius).toFloat
+
+      for( item <- states ) {
         val cursor: HCursor = item.hcursor
         val list: List[Json] = cursor.values.get.toList
-        val altitude: Json = list(7)
+        val longtitude: Float = list(5).toString.toFloat
+        val lattitude: Float = list(6).toString.toFloat
 
-        if (altitude.isNumber){
-          val number: Double = altitude.toString.toDouble
-          if (number > maxAltitude) {
-            maxAltitude = number
+        if (longtitude <= lomin && longtitude >= lomax){
+          if (lattitude <= lamin && lattitude >= lamax){
+            "add params to the url and send it to ingest actor or think how it can be done"
           }
         }
       }
-      println(timestamp + ": " + maxAltitude)
     }
-    catch {
-      case error: Exception => error.printStackTrace()
-    }
-
-    1//!TEMPORARY!
-  }
-
-  def findHighestSpeed(data: Json): Int = {
-    1//!TEMPORARY!
-  }
-
-  def findCountOfAirplanes(data: Json): Int = {
     1//!TEMPORARY!
   }
 
